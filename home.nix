ca9afbbc6a119a812db5e30ee27083e50525b9dd
moonpiedumplings <jeff.fonseca777@gmail.com>
2024-08-04 23:15:48 -0700

    New starting point, using flakes

--- home.nix
+++ home.nix
@@ -1,27 +1,10 @@
-{
-  pkgs ? import <nixpkgs> {},
-  config,
-  lib,
-  ... 
-}:
-let
-  nixgl = import <nixgl> {};
-in
-with import ./quarto.nix {inherit pkgs config lib;};
-with import ./nixglwrapper.nix {inherit pkgs config lib nixgl;};
+{ config, pkgs, ... }:
+
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "moonpie";
   home.homeDirectory = "/home/moonpie";
-  targets.genericLinux.enable = true;
-
-  nixpkgs.config.allowUnfree = true;
-  nix = {
-    settings = {
-    experimental-features = ["nix-command" "flakes"];
-    };
-  };
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -30,62 +13,15 @@ with import ./nixglwrapper.nix {inherit pkgs config lib nixgl;};
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  nix.package = pkgs.nix;
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  #fonts.fontconfig.enable = true;
-  xdg.mime.enable = true;
   home.packages = [
-    #nixgl
-    nixgl.nixGLIntel
-    nixgl.nixVulkanIntel
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
 
-    #nixgl wrapped stuff
-    (nixGLWrap pkgs.vscode)
-    (nixGLWrap pkgs.microsoft-edge)
-    (nixGLWrap pkgs.firefox-bin)
-
-
-    #general tools and utilities
-    pkgs.micro
-    pkgs.calibre
-    pkgs.languagetool
-    pkgs.htop
-
-    #git tools
-    pkgs.git
-    pkgs.bfg-repo-cleaner
-    pkgs.git-filter-repo
-
-    #share sound with android devices.
-    pkgs.soundwireserver
-
-    # development enviroment stuff
-    quarto # see the imports above.
-    (pkgs.python311.withPackages(ps: with ps; [ jupyter ]))
-    pkgs.poetry
-
-    # general cli tools
-    pkgs.yt-dlp
-
-    #hacking
-    pkgs.macchanger
-    pkgs.nmap
-    pkgs.wireshark
-    pkgs.metasploit
-    pkgs.aircrack-ng
-    pkgs.mdk4
-    pkgs.airgeddon
-
-    #creativity
-    pkgs.manuskript
-
-    # storage and encryption
-    pkgs.rclone
-    pkgs.gocryptfs
-    pkgs.syncthing
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -102,16 +38,39 @@ with import ./nixglwrapper.nix {inherit pkgs config lib nixgl;};
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
-  home.file = {};
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/moonpie/etc/profile.d/hm-session-vars.sh
+  #
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
-  programs = {
-  	home-manager.enable = true;
-  	bash.enable = true;
-  	gh.enable = true;
-  };
-  services.syncthing.enable = true;
+  programs.home-manager.enable = true;
 }
