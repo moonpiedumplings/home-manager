90d9250041fe7f3b686497ef419742cb3a6b6a1c
moonpiedumplings <jeff.fonseca777@gmail.com>
2024-09-12 13:25:30 -0700

    gzdoom and nixgl

--- flake.nix
+++ flake.nix
@@ -4,19 +4,36 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pkgs-kubectl.url = "github:nixos/nixpkgs/7a339d87931bba829f68e94621536cad9132971a";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      # inputs.nixpkgs.follows = "nixpkgs";
+      };
   };
 
-  outputs = { nixpkgs, home-manager, ... }:
+  outputs = {
+            nixgl,
+            nixpkgs, pkgs-kubectl,
+            home-manager, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ nixgl.overlay ];
+        };
+      # pkgs-kbctl = pkgs-kubectl.legacyPackages.${system};
+
     in {
       homeConfigurations."moonpie" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
+
+        extraSpecialArgs = {
+          pkgs-kbctl = import pkgs-kubectl {inherit system;};
+        };
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
@@ -24,6 +41,7 @@
 
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
+
       };
     };
 }
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-kbctl, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
@@ -18,22 +18,11 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    pkgs-kbctl.kubectl
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.fluxcd
+    pkgs.nixgl.nixGLIntel pkgs.nixgl.nixVulkanIntel
+    pkgs.gzdoom
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
